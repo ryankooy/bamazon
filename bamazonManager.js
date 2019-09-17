cd1b7733f47a530fc5f8855cb7eb06ab11f0f721
Ryan Kooy <rwkooy@gmail.com>
2019-09-17 04:57:05 -0400

    creating bamazonManager functions

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,4 +1,4 @@
-var customer = require("./bamazonCustomer.js");
+// var customer = require("./bamazonCustomer.js");
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
@@ -12,6 +12,7 @@ var conn = mysql.createConnection({
 
 conn.connect(function(err) {
     if(err) throw err;
+    menu();
 });
 
 function menu() {
@@ -22,14 +23,48 @@ function menu() {
             choices: ["VIEW PRODUCTS FOR SALE", "VIEW LOW INVENTORY", "ADD TO INVENTORY", "ADD NEW PRODUCT"]
         }
     ]).then(function(ans) {
-        if(ans.options === choices[0]) {
-            console.log("Hi.");
-        } else if(ans.options === choices[1]) {
-            console.log("Goodbye.");
-        } else if(ans.options === choices[2]) {
-            console.log("Oh, hi!");
+        if(ans.options === "VIEW PRODUCTS FOR SALE") {
+            forSale();
+        } else if(ans.options === "VIEW LOW INVENTORY") {
+            lowInv();
+        } else if(ans.options === "ADD TO INVENTORY") {
+            addInv();
         } else {
-            console.log("Ok, bye.");
+            newProd();
         }
     });
+}
+
+function forSale() {
+    var sql = "select * from products";
+    conn.query(sql, function(err, res) {
+        if(err) throw err;
+        console.log("-----------------------------------------------\nITEM ID | PRODUCT | DEPARTMENT | PRICE | QUANTITY");
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        }
+        console.log("-----------------------------------------------");
+        menu();
+    });
+}
+
+function lowInv() {
+    var sql = "select * from products having stock_quantity < 5";
+    conn.query(sql, function(err, res) {
+        if(err) throw err;
+        console.log("-----------------------------------------------\nITEM ID | PRODUCT | DEPARTMENT | PRICE | QUANTITY");
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        }
+        console.log("-----------------------------------------------");
+        menu();
+    });
+}
+
+function addInv() {
+
+}
+
+function newProd() {
+
 }
